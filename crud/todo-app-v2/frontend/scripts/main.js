db0febd8b0284cc08148afafd80cf28342df04b5
Acto <acto@Actos-MacBook-Air.local>
2023-12-23 13:28:34 +0100

    added delete and complete features to todo app v2

--- crud/todo-app-v2/frontend/scripts/main.js
+++ crud/todo-app-v2/frontend/scripts/main.js
@@ -1,94 +1,167 @@
-const addTodoBtn = document.getElementById("addTodoBtn");
-addTodoBtn.addEventListener("click", async function addTodo() {
-  const newTodoInput = document.getElementById("newTodo");
-  const todoList = document.getElementById("todoList");
+import { deleteTodo, fetchTodos, completeTodo, addTodoItem } from "./todoservice.js";
 
-  if (newTodoInput.value.trim() !== "") {
-    const todoText = newTodoInput.value.trim();
+document.addEventListener("DOMContentLoaded", () => {
+  // Attach event listeners to dynamically created elements
+  document.addEventListener("click", handleButtonClick);
 
-    try {
-      const response = await axios.post("http://localhost:5000/todos", {
-        text: todoText,
-      });
-      const todoItem = createTodoItem(response.data.id, response.data.text);
-      todoList.appendChild(todoItem);
-      newTodoInput.value = "";
-    } catch (error) {
-      console.error("Error adding todo:", error);
-    }
-  }
+  // Fetch and display initial todo list
+  displayTodosInUI();
 });
 
-async function deleteTodo(id, todoItem) {
+async function displayTodosInUI() {
+  // Get current todo list element
+  const todoList = document.getElementById("todoList");
+
+  // Clear existing content
+  todoList.innerHTML = "";
+
+  // Get todo items from db
+  let todos;
+
   try {
-    const response = await axios.delete(`http://localhost:5000/todos/${id}`);
-    if (response.status === 200) {
-      todoList.removeChild(todoItem);
-    } else {
-      console.error("Error deleting todo:", response.statusText);
-    }
+    todos = await fetchTodos();
   } catch (error) {
-    console.error("Error deleting todo:", error);
+    console.error("Error fetching todos:", error);
+    return;
+  }
+
+  // Create and append elements for each todo
+  todos.forEach((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.className = "todoItem";
+    todoItem.setAttribute("id", `${todo.id}`);
+
+    // const checkbox = document.createElement("input");
+    // checkbox.type = "checkbox";
+
+    const todoText = document.createElement("span");
+    todoText.textContent = todo.text;
+
+    const completeButton = document.createElement("button");
+    completeButton.textContent = "complete";
+    completeButton.setAttribute("id", "completeBtn");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.setAttribute("id", "deleteBtn");
+
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(completeButton);
+    todoItem.appendChild(deleteButton);
+
+    todoList.appendChild(todoItem);
+  });
+}
+
+async function handleButtonClick(event) {
+  const target = event.target;
+
+  // Check if the clicked element is a "Add" button
+  if (target.id === "addTodoBtn") {
+    const todoList = document.getElementById("todoList");
+    const newTodoInput = document.getElementById("newTodoInput");
+    if (newTodoInput.value.trim() !== "") {
+      const todoText = newTodoInput.value.trim();
+      let response;
+      try {
+        response = await addTodoItem(todoText);
+        if (response.status === 200) {
+          const todoItem = createTodoItem(response.data.id, response.data.text);
+          todoList.appendChild(todoItem);
+          newTodoInput.value = "";
+        }
+      } catch (error) {
+        console.error("Error adding todo:", error);
+      }
+    }
+  }
+
+  // Check if the clicked element is a "Delete" button
+  if (target.id === "deleteBtn") {
+    const todoList = document.getElementById("todoList");
+
+    // get parent node and it's id
+    const parent = event.target.parentNode;
+    const parentId = event.target.parentNode.id;
+    let response;
+
+    try {
+      response = await deleteTodo(parentId);
+      if (response.status === 200) {
+        todoList.removeChild(parent);
+      } else {
+        console.error("Error deleting todo:", response.statusText);
+      }
+    } catch (error) {
+      console.log("error deleting todo");
+    }
+  }
+
+  // Check if the clicked element is a "Complete" button
+  if (target.id === "completeBtn") {
+    // get parent node and it's id
+    const parent = event.target.parentNode;
+    const todoText = parent.children[0];
+    const parentId = event.target.parentNode.id;
+    let response;
+
+    try{
+      response = await completeTodo(parentId);
+      if (response.status === 200) {
+        todoText.classList.add("completed");
+      } else {
+        console.error("Error completing todo:", response.statusText);
+      }
+    } catch (error) {
+      console.log("error completing todo");
+    }
+  }
+
+  // Check if the clicked element is an "Edit" button
+  if (target.classList.contains("edit-button")) {
+    const todoId = target.dataset.id;
+    const newText = prompt("Enter the new text for the todo:");
+
+    if (newText !== null) {
+      try {
+        const updatedTodo = await updateTodo(todoId, newText);
+        // Update the UI to reflect the changes
+        updateTodoInUI(updatedTodo);
+      } catch (error) {
+        // Handle errors, e.g., display an error message to the user
+        console.error("Error updating todo:", error);
+      }
+    }
   }
 }
 
 function createTodoItem(id, text) {
   const todoItem = document.createElement("li");
   todoItem.className = "todoItem";
+  todoItem.setAttribute("id", `${id}`);
 
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  // const checkbox = document.createElement("input");
+  // checkbox.type = "checkbox";
 
   const todoText = document.createElement("span");
   todoText.textContent = text;
 
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "complete";
+  completeButton.setAttribute("id", "completeBtn");
+
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
-  deleteButton.onclick = function () {
-    deleteTodo(id, todoItem);
-  };
+  deleteButton.setAttribute("id", "deleteBtn");
 
-  todoItem.appendChild(checkbox);
   todoItem.appendChild(todoText);
+  todoItem.appendChild(completeButton);
   todoItem.appendChild(deleteButton);
 
   return todoItem;
 }
 
-// const addTodoBtn = document.getElementById("addTodoBtn");
-// addTodoBtn.addEventListener("click", function () {
-//   const newTodoInput = document.getElementById("newTodo");
-//   const todoList = document.getElementById("todoList");
-
-//   if (newTodoInput.value.trim() !== "") {
-//     const todoItem = document.createElement("li");
-//     todoItem.className = "todoItem";
-
-//     const checkbox = document.createElement("input");
-//     checkbox.type = "checkbox";
-
-//     checkbox.addEventListener("change", function () {
-//       if (checkbox.checked) {
-//         console.log("Task completed:", todoText.textContent);
-//       } else {
-//         console.log("Task marked as incomplete:", todoText.textContent);
-//       }
-//     });
-
-//     const todoText = document.createElement("span");
-//     todoText.textContent = newTodoInput.value;
-
-//     const deleteButton = document.createElement("button");
-//     deleteButton.textContent = "Delete";
-//     deleteButton.onclick = function () {
-//       todoList.removeChild(todoItem);
-//     };
-
-//     todoItem.appendChild(checkbox);
-//     todoItem.appendChild(todoText);
-//     todoItem.appendChild(deleteButton);
-
-//     todoList.appendChild(todoItem);
-//     newTodoInput.value = "";
-//   }
-// });
+function updateTodoInUI(updatedTodo) {
+  // Implement logic to update the UI with the modified todo
+  // This might involve finding the todo element in the DOM and updating its text
+}
---
+++ crud/todo-app-v2/frontend/scripts/todoservice.js
@@ -0,0 +1,56 @@
+const API_URL = "http://localhost:5000/todos";
+
+async function fetchTodos() {
+  try {
+    // Make a GET request to fetch todos from the server
+    const response = await axios.get(`${API_URL}`);
+    const todos = response.data;
+    return todos;
+  } catch (error) {
+    // If there's an error, reject the promise with the error
+    console.log("there's an Error");
+    throw error;
+  }
+}
+
+async function addTodoItem(todoText) {
+  try {
+    const response = await axios.post(`${API_URL}`, {
+      text: todoText,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error adding todo:", error);
+    throw error;
+  }
+}
+
+async function completeTodo(id) {
+  try {
+    const response = await axios.put(`${API_URL}/${id}`);
+    return response;
+  } catch (error) {
+    console.error("Error completing todo:", error);
+  }
+}
+
+// async function updateTodo(id, newText) {
+//   try {
+//     const response = await axios.put(`${API_URL}/${id}`, { text: newText });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error updating todo:", error);
+//     throw error;
+//   }
+// }
+
+async function deleteTodo(id) {
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response;
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+  }
+}
+
+export { fetchTodos, deleteTodo, completeTodo, addTodoItem };
